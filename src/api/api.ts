afb2f75ccf9f3c82b66c9ebf261ffecde65d8b4e
malimova <nemarina73@list.ru>
2026-03-17 17:53:53 +0400

    fix deploy

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api',
+  baseURL: import.meta.env.VITE_API_BASE_URL,
   headers: {
     'X-Auth-Token': import.meta.env.VITE_API_KEY.trim()
   }
--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+/// <reference types="vite/client" />
 import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 import dotenv from 'dotenv'  
@@ -18,18 +19,5 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/test/setup.ts',
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: process.env.FOOTBALL_DATA_API_BASE_URL,
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-        headers: {
-          'X-Auth-Token': process.env.VITE_API_KEY?.trim() || ''
-        }
-      }
-    }
   }
 })
