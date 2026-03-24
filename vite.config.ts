4eec652982a992a9f137271f480c66f316b63eff
malimova <nemarina73@list.ru>
2026-03-24 21:11:55 +0400

    fixed vite.config without dotenv

--- vite.config.ts
+++ vite.config.ts
@@ -1,33 +1,33 @@
-/// <reference types="vite/client" />
-import { defineConfig } from 'vitest/config'
+/// <reference types="vitest" />
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
-import dotenv from 'dotenv'  
-import { fileURLToPath } from 'url'
-import { dirname, resolve } from 'path'
 
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = dirname(__filename)
-dotenv.config({ path: resolve(__dirname, '.env') })
-
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'dist',
-  },
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/test/setup.ts',
-  },
-  server: {
-    proxy: {
-      '/api': {
-        target: process.env.FOOTBALL_DATA_API_BASE_URL,
-        changeOrigin: true,
-        secure: false,
-        rewrite: (path) => path.replace(/^\/api/, ''),
-        headers: {
-          'X-Auth-Token': process.env.VITE_API_KEY?.trim() || ''
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  
+  return {
+    plugins: [react()],
+    
+    build: {
+      outDir: 'dist',
+    },
+    
+    test: {
+      globals: true,
+      environment: 'jsdom',
+      setupFiles: './src/test/setup.ts',
+    },
+    
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_BASE_URL ,
+          changeOrigin: true,
+          secure: false,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+          headers: {
+            'X-Auth-Token': env.VITE_API_KEY?.trim() || ''
+          }
         }
       }
     }
